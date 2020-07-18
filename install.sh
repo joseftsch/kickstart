27132b3b4730751439b2e6811b400e80f66b77b5
Josef Tschiggerl <josef@tschiggerl.name>
2020-07-18 14:23:14 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#jtl server install script for Centos 8
+servername=$(hostname)
+
+rm -f /root/.ssh/id_ed25519
+rm -f /root/.ssh/id_ed25519.pub
+ssh-keygen -o -a 200 -t ed25519 -f /root/.ssh/id_ed25519 -q -N "" -C "${servername} github puppet deployment"
+
+echo "Enter the following pub key to https://github.com/joseftsch/puppet/settings/keys:"
+cat /root/.ssh/id_ed25519.pub
+echo ""
+read -p "Press any key once key added to continue with setup .. " -n1 -s
+echo ""
+
+yum clean all
+#lets not use puppet6 for now ...
+#rpm -Uvh https://yum.puppet.com/puppet6-release-el-8.noarch.rpm
+rpm -Uvh https://yum.puppet.com/puppet5-release-el-8.noarch.rpm
+yum install puppet-agent git -y
+
+rm -rf /etc/puppetlabs/code
+cd /etc/puppetlabs/
+git clone git@github.com:joseftsch/puppet.git code
+
+#run puppet
+while true; do
+    read -p "Do you wish to run puppet? (y/n)" yn
+    case $yn in
+        [Yy]* ) /opt/puppetlabs/bin/puppet apply -l /var/log/puppetlabs/puppet/puppet.log < /dev/null; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+#reboot
+while true; do
+    read -p "Do you wish to reboot? (y/n)" yn
+    case $yn in
+        [Yy]* ) reboot; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
